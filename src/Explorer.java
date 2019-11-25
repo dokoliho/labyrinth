05150af3e632300968d4b42701d3f3874dc4ce63
Oliver Hofmann <oliver.hofmann@th-nuernberg.de>
2019-11-25 23:31:11 +0100

    Running

---
+++ src/Explorer.java
@@ -0,0 +1,178 @@
+
+public class Explorer {
+
+    private final class Heading {
+        public static final int NORTH = 0;
+        public static final int EAST = 3;
+        public static final int SOUTH = 6;
+        public static final int WEST = 9;
+    }
+
+    private class Position {
+        public int x = 0;
+        public int y = 0;
+    }
+
+    private Maze maze;
+    private Position pos;
+    private int heading;
+    private String[] mazeMap;
+
+    public String exploreMaze(Maze z) {
+        maze = z;
+        pos = new Position();
+        heading = Heading.SOUTH;
+        boolean done = false;
+        initStringArray(z);
+
+        do {
+            int exitCount = getExitCount();
+            if (exitCount>2)
+                done = doJunction(getExits(exitCount));
+            else
+                doPathway();
+        } while (!done);
+
+        return String.join("\n", mazeMap);
+    }
+
+    private void initStringArray(Maze z) {
+        mazeMap = new String[z.height()+2];
+        mazeMap[0] = mazeMap[mazeMap.length-1] = "#".repeat(z.width()+2);
+        for (int i=1; i<mazeMap.length-1;i++)
+            mazeMap[i] = "#" + " ".repeat(z.width()) + "#";
+    }
+
+    private boolean doJunction(int[] exits) {
+        if (sumOfMarks() == 0) {
+            markAndWalk(exits[0]); // BACK ist in jedem Fall der letzte Exit
+            return false;
+        }
+        if (maze.marks(Maze.Direction.BACK) == 0) {
+            markAndWalk(Maze.Direction.BACK);
+            return false;
+        }
+        int exit = minExit(exits);
+        if (exit == -1) return true;
+        markAndWalk(exit);
+        return false;
+    }
+
+    private int sumOfMarks() {
+        int result = 0;
+        for (int i = Maze.Direction.LEFT; i <= Maze.Direction.BACK; i++)
+            result += maze.marks(i);
+        return result;
+    }
+
+    private int minExit(int[] exits) {
+        int minMarkCount = 2;
+        int result = -1;
+        for (int i=0; i<exits.length; i++)
+            if (maze.marks(exits[i])<minMarkCount) {
+                minMarkCount = maze.marks(exits[i]);
+                result = exits[i];
+            }
+        return result;
+    }
+
+    private void markAndWalk(int direction) {
+        maze.mark(Maze.Direction.BACK);
+        maze.mark(direction);
+        walk(direction);
+    }
+
+
+    private void doPathway() {
+        for (int i = Maze.Direction.LEFT; i <= Maze.Direction.BACK; i++)
+            if (!isWall(i)) {
+                walk(i);
+                return;
+            }
+    }
+
+    private int[] getExits(int exitCount) {
+        int[] result = new int[exitCount];
+        int count = 0;
+        for (int i = Maze.Direction.LEFT; i <= Maze.Direction.BACK; i++)
+            if (!isWall(i))
+                result[count++] = i;
+        return result;
+    }
+
+    private int getExitCount() {
+        int exitCount = 0;
+        for (int i = Maze.Direction.LEFT; i <= Maze.Direction.BACK; i++)
+            if (!isWall(i))
+                exitCount++;
+        return exitCount;
+    }
+
+    private void walk(int direction) {
+        pos = next(direction);
+        heading = headingAfterTurn(direction);
+        maze.walk(direction);
+    }
+
+
+    private Position next(int direction) {
+        Position result = new Position();
+        result.x = pos.x;
+        result.y = pos.y;
+        int heading = headingAfterTurn(direction);
+        switch(heading) {
+            case Heading.NORTH:
+                result.y--;
+                break;
+            case Heading.EAST:
+                result.x++;
+                break;
+            case Heading.SOUTH:
+                result.y++;
+                break;
+            case Heading.WEST:
+                result.x--;
+                break;
+        }
+        return result;
+    }
+
+    private int turnLeft(int heading) {
+        return (heading + 9) % 12;
+    }
+
+    private int turnRight(int heading) {
+        return (heading + 3) % 12;
+    }
+
+    private int headingAfterTurn(int direction) {
+        switch (direction) {
+            case Maze.Direction.LEFT:
+                return turnLeft(heading);
+            case Maze.Direction.AHEAD:
+                return heading;
+            case Maze.Direction.RIGHT:
+                return turnRight(heading);
+            case Maze.Direction.BACK:
+                return turnRight(turnRight(heading));
+        }
+        return heading;
+    }
+
+    private boolean isWall(int direction) {
+        boolean result = maze.isWall(direction);
+        Position pos = next(direction);
+        StringBuilder sb = new StringBuilder(mazeMap[pos.y+1]);
+        sb.setCharAt(pos.x+1, (result?'#':'.'));
+        mazeMap[pos.y+1] = sb.toString();
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        Maze m = new Maze();
+        Explorer e = new Explorer();
+        System.out.println(e.exploreMaze(m));
+    }
+
+}
--- src/Maze.java
+++ src/Maze.java
@@ -5,10 +5,20 @@ public class Maze {
         public int y = 0;
     }
 
-    private static final int LEFT = 0;
-    private static final int AHEAD = 1;
-    private static final int RIGHT = 2;
-    private static final int BACK = 3;
+    public final class Direction {
+        public static final int LEFT = 0;
+        public static final int AHEAD = 1;
+        public static final int RIGHT = 2;
+        public static final int BACK = 3;
+    }
+
+    public final class Bearing {
+        public static final int NORTH = 0;
+        public static final int EAST = 3;
+        public static final int SOUTH = 6;
+        public static final int WEST = 9;
+    }
+
 
     private static final String[] example01 = {
             "           ",
@@ -24,60 +34,117 @@ public class Maze {
             "           "
     };
 
-    private String[] maze;
+    private boolean[][] maze;
+    private int[][] mark;
     private Position pos = new Position();
-    private int heading = 6; // down
+    private int heading = Bearing.SOUTH;
 
     public Maze() {
-        maze = example01;
+        init(example01);
     }
 
+    private void init(String[] layout) {
+        maze = new boolean[layout.length][layout[0].length()];
+        mark = new int[layout.length][layout[0].length()];
+        for (int row = 0; row < layout.length; row++)
+            for (int col = 0; col < layout[row].length(); col++) {
+                maze[row][col] = layout[row].charAt(col) == '#';
+                mark[row][col] = 0;
+            }
+    }
+
+    private int getMarks(Position p, int bearing) {
+        int b = mark[p.y][p.x];
+        switch(bearing) {
+            case Bearing.NORTH:
+                break;
+            case Bearing.EAST:
+                b = b>>2;
+                break;
+            case Bearing.SOUTH:
+                b = b>>4;
+                break;
+            case Bearing.WEST:
+                b = b>>6;
+                break;
+        }
+        return b & 3;
+    }
+
+    private void setMarks(Position p, int bearing, int count) {
+        int mask = 3;
+        switch(bearing) {
+            case Bearing.NORTH:
+                break;
+            case Bearing.EAST:
+                count = count<<2;
+                mask = mask<<2;
+                break;
+            case Bearing.SOUTH:
+                count = count<<4;
+                mask = mask<<4;
+                break;
+            case Bearing.WEST:
+                count = count<<6;
+                mask = mask<<6;
+                break;
+        }
+        mask = ~mask;
+        mark[p.y][p.x] = (mark[p.y][p.x] & mask) + count;
+    }
+
+
     public int width() {
-        return maze[0].length()+2;
+        return maze[0].length;
     }
 
     public int height() {
-        return maze.length+2;
+        return maze.length;
+    }
+
+    private int turnLeft(int startDir) {
+        return (startDir + 9) % 12;
+    }
+
+    private int turnRight(int startDir) {
+        return (startDir + 3) % 12;
     }
 
     private int turn(int direction) {
-        int result = heading;
         switch (direction) {
-            case LEFT:
-                result += 9;
-                break;
-            case AHEAD:
-                break;
-            case RIGHT:
-                result += 3;
-                break;
-            case BACK:
-                result += 6;
-                break;
+            case Direction.LEFT:
+                return turnLeft(heading);
+            case Direction.AHEAD:
+                return heading;
+            case Direction.RIGHT:
+                return turnRight(heading);
+            case Direction.BACK:
+                return turnRight(turnRight(heading));
         }
-        result = result % 12;
-        return result;
+        return heading;
     }
+
 
     private Position next(int direction) {
         Position result = new Position();
         result.x = pos.x;
         result.y = pos.y;
-        int dir = turn(direction);
-        switch(dir) {
-            case 0:
+        int bearing = turn(direction);
+        switch(bearing) {
+            case Bearing.NORTH:
                 result.y--;
                 break;
-            case 3:
+            case Bearing.EAST:
                 result.x++;
                 break;
-            case 6:
+            case Bearing.SOUTH:
                 result.y++;
                 break;
-            case 9:
+            case Bearing.WEST:
                 result.x--;
                 break;
         }
+        return result;
     }
 
     public boolean isWall(int direction) {
@@ -85,19 +152,32 @@ public class Maze {
         return isWall(p);
     }
 
-    public boolean isWall(Position p) {
-        if (p.x<0 || p.x>=maze[0].length()) return true;
+    private boolean isWall(Position p) {
+        if (p.x<0 || p.x>=maze[0].length) return true;
         if (p.y<0 || p.y>=maze.length) return true;
-        return maze[p.y].charAt(p.x)=='#';
+        return maze[p.y][p.x];
     }
 
     public void walk(int direction) {
         Position p = next(direction);
-        if (isWall(p)) return;
+        if (isWall(p)) {
+            System.out.println("Boing");
+            return;
+        }
         heading = turn(direction);
         pos = p;
+        // System.out.println("Maze x " + pos.x + " y " + pos.y + " heading " + heading);
     }
 
+    public void mark(int direction) {
+        int bearing = turn(direction);
+        setMarks(pos, bearing, getMarks(pos, bearing)+1);
+    }
+
+    public int marks(int direction) {
+        int bearing = turn(direction);
+        return getMarks(pos, bearing);
+    }
 
 
 }
